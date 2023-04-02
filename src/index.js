af401ab5387588fd10f0faf428c4d617fbcc51eb
Yash Gandhi <yash4gandhi@gmail.com>
2023-04-01 20:08:26 -0700

    change name

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ class App extends React.Component{
     render(){
         return (
             <div className='App'>
-                <h1>Hello, World</h1>
+                <h1>Hello, Hi</h1>
             </div>
         )
     }
